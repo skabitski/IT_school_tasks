a80df2ca3ebf6fa3d2d612445d0c76d9548893a3
Maks_Skabitski <mskabitskiy@gmail.com>
2024-03-06 16:06:17 +0200

    week 17

---
+++ 17 week/17week.js
@@ -0,0 +1,38 @@
+// let str = "Я живу в Москве";
+// let pos = str.indexOf("Москве");
+// console.log;
+
+// str = "Пожалуйста, посетите веб-сайт Microsoft!";
+// let n = str.replace("Microsoft", "Google");
+
+// let now = new Date()
+// alert(now)
+
+
+function transformFullName() {
+   
+    let fullName = document.getElementById('fullName').value.trim();
+
+    
+    let parts = fullName.split(/\s+/).filter(Boolean);
+
+   
+    if (parts.length >= 3) {
+       
+        let firstName = capitalizeFirstLetter(parts[1]);
+        let lastName = capitalizeFirstLetter(parts[0]);
+        let patronymic = capitalizeFirstLetter(parts.slice(2).join(' '));
+
+        
+        document.getElementById('result').innerHTML = 'Имя: ' + firstName + '<br>' +
+                                                        'Фамилия: ' + lastName + '<br>' +
+                                                        'Отчество: ' + patronymic;
+    } else {
+        
+        document.getElementById('result').innerHTML = 'Некорректный ввод';
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
